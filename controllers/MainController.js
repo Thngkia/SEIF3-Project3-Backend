359c52c041fa22a94459df479c810093b0427874
Jeremy <jeremyncc94@hotmail.com>
2020-12-05 14:14:14 +0800

    added colors for each cluster

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -11,13 +11,18 @@ const controllers = {
         })
             .then(result => {
                 // clean up api by converting to a Json obj and then into an array from api
+                // res.send(JSON.parse(result.data).SrchResults.slice(1))
                 let clustersApi = (JSON.parse(result.data)).SrchResults.slice(1)
                 // initialise full coords array
                 let fullCoords = []
                 // loop through all the clusters api
                 clustersApi.forEach(item => {
                     // init array for each hot spot
-                    let oneSpotCoords = []
+                    let oneSpotCoords = {
+                        size: item["CASE_SIZE"],
+                        color: null,
+                        coordsArr: []
+                    }
                     // loop through each hot spot to and split the latlng by "|"
                     item.LatLng.split("|").forEach(item => {
                         let coordsArr = item.split(",")
@@ -26,11 +31,21 @@ const controllers = {
                             "lng": Number(coordsArr[1])
                         }
                         // push into each spot
-                        oneSpotCoords.push(obj)
+                        oneSpotCoords.coordsArr.push(obj)
                     })
                     // push into full coords
                     fullCoords.push(oneSpotCoords)
                 })
+
+                // set color based on size
+                fullCoords.forEach (item => {
+                    if (item.size > 9) {
+                        item.color = "red"
+                    } else {
+                        item.color = "yellow"
+                    }
+                })
+                // console.log(fullCoords)
                 res.send(fullCoords)
             })
     },
@@ -113,6 +128,7 @@ function getMinimumDistanceToRiskArea(riskAreas, lat) {
  * @return minimum distance is in meter and is within risk area or not.
  */
 function findSafeDistanceFromRiskArea(riskAreaLocations, currentLocation) {
+    console.log(currentLocation)
     let isWithinRiskArea = false
     let minimumDistance = 0
     const SAFE_DISTANCE_FROM_RISK_AREA = 150
@@ -137,10 +153,12 @@ function findSafeDistanceFromRiskArea(riskAreaLocations, currentLocation) {
  */
 function calculateDistance(fromPoint, toPoint) {
 
-    fromPointLat = fromPoint.split(",")[0]
-    fromPointLng = fromPoint.split(",")[1]
-    toPointLat = toPoint.split(",")[0]
-    toPointLng = toPoint.split(",")[1]
+    console.log(toPoint.split(",")[0], "calcualte distance")
+
+    let fromPointLat = fromPoint.split(",")[0]
+    let fromPointLng = fromPoint.split(",")[1]
+    let toPointLat = toPoint.split(",")[0]
+    let toPointLng = toPoint.split(",")[1]
 
     //First point in your haversine calculation
     const point1 = { lat: fromPointLat, lng: fromPointLng }
